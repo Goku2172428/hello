a30ca95faaa36c47b8dc9c6a85b3fa02d4a5ac04
Goku2172428 <133489010+Goku2172428@users.noreply.github.com>
2024-04-30 14:21:38 +0530

    ascii.java

---
+++ ascii.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ascii {
+    public static void main(String[] args) {
+       int num=4;
+       solve(num--);
+        System.out.println(num);
+    }
+
+    private static void solve(int num) {
+        System.out.println(num);
+    }
+}
